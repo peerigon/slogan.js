0e1a621b17929b70bee30005c48c64b1de4e3953
meaku <michael.jaser@peerigon.com>
2015-02-17 23:40:58 +0100

    Add charset

--- bin/app.js
+++ bin/app.js
@@ -4,7 +4,7 @@ var http = require("http"),
     slogan = require("../index");
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.writeHead(200, {"Content-Type": "text/plain; charset=utf8"});
     res.end(slogan("peerigon") + "\n");
 }).listen(7177, "127.0.0.1");
 
