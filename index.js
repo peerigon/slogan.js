c63ed8b7dda24ee11353b435deab5d5d7152a553
Carsten <acidicX@users.noreply.github.com>
2018-09-12 14:57:30 +0200

    make slogenjs great again

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ var slogans = [
     "— Das Auto",
     "— Qualitysoftware since 1784",
     "- flieg und sieg!",
-    "- Go! Go! Go!"
+    "- Go! Go! Go!",
     "- not just gently raising the bar of excellence"
 ];
 
